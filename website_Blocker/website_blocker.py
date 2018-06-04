8b2b718259155ee2838f97ec8128bd3cf4e1a2ce
Renish <rk080299@gmail.com>
2018-06-04 22:31:09 +0530

    website_blocker

---
+++ website_Blocker/website_blocker.py
@@ -0,0 +1,33 @@
+import time
+from datetime import datetime as dt
+
+
+hosts_path = "/etc/hosts"
+
+redirect_ip = "127.0.0.1"
+
+website_list=["www.google.com","www.facebook.com"]
+
+
+while True:
+
+	if dt(dt.now().year,dt.now().month,dt.now().day,13) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,23):
+		print("working hours...")
+		with open(hosts_path,'r+') as file:
+			content = file.read()
+			for website in website_list:
+				if website in content:
+					pass
+				else:
+					file.write(redirect_ip + " " + website +'\n')
+	else:
+		with open(hosts_path,'r+') as file:
+			content_lines = file.readlines()
+			file.seek(0)
+			for line in content_lines:
+				if not any(website in line for website in website_list):
+					file.write(line)
+
+			file.truncate()
+		print("fun time...")
+	time.sleep(5)
